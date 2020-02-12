ad368c195781ed380534216d8711e178dd877490
trcasper <trcasper90@gmail.com>
2020-02-12 14:47:16 -0700

    Contact page and queries

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -23,49 +23,71 @@ class Landing extends React.Component {
               Sound
             </div>
           </div>
+        </div>
+        <div className="Nav">
+          <div className="HomeLink">
+            <a href="#Intro">Home</a>
           </div>
-          <div className="Nav">
-            <div className="HomeLink">
-              <a href="#Intro">Home</a>
-            </div>
-            <div id="AboutLink">
-              <a href="#About">About</a>
-            </div>
-            <div id="InsideLink">
+          <div id="AboutLink">
+            <a href="#About">About</a>
+          </div>
+          <div id="InsideLink">
             <a href="#InsideScroll">Inside</a>
-            </div>
-            <div id="ContactLink">
-            <a href="#ContactScroll">Contact</a>
-            </div>
-            <div id="EventsLink">
-            <a href="#Events">Events</a>
-            </div>
           </div>
+          <div id="ContactLink">
+            <a href="#ContactScroll">Contact</a>
+          </div>
+          <div id="EventsLink">
+            <a href="https://www.facebook.com/pg/boardwalksound/events/?ref=page_internal"
+            target="_blank">Events</a>
+          </div>
+        </div>
 
-          <div className="IntroPicContainer">
-            <img
-              className="BoardwalkOne"
-              src={BoardwalkOne}
-              alt="BoardwalkOneAlt"
-            />
+        <div className="IntroPicContainer">
+          <img
+            className="BoardwalkOne"
+            src={BoardwalkOne}
+            alt="BoardwalkOneAlt"
+          />
           {/* </div> */}
         </div>
         <div id="About">
-            {/* <div className="AboutTitle">About</div> */}
-            <div className="AboutContent">
-                <div className="AboutHeadlineOne">Great Sound, Great Staff,<br/>Book Your Event!</div>
-
-                <div className="AboutInfo">Our Professionally designed venue was created by Logan Casper and Braxton Nelson.  It was constructed in 2020 to bring a state of the art sound to the local music scene.  With the gear and flexibility to accommodate any type of event it is perfect for concerts, parties, or gatherings at a central location in Utah Valley. The staff is friendly and happy to help bring your vision to life.</div>
-                
+          {/* <div className="AboutTitle">About</div> */}
+          <div className="AboutContent">
+            <div className="AboutHeadlineOne">
+              Great Sound, Great Staff,
+              <br />
+              Book Your Event!
             </div>
+
+            <div className="AboutInfo">
+              Our Professionally designed live recording studio was created by Logan Casper and
+              Braxton Bitsoi. It was constructed in 2020 to bring a state of the
+              art sound to the local music scene. With the gear and flexibility
+              to accommodate all types of artists, it is built to fit your needs. With a central location in Utah valley, our
+              staff is friendly and happy to help bring your vision to life.
+            </div>
+          </div>
         </div>
         <div id="InsideScroll"></div>
         <div className="Inside">
-            <div className="InsideHeadline">Take a look Inside.</div>
+          <div className="InsideHeadline">Take a look Inside.</div>
         </div>
         <div id="ContactScroll"></div>
         <div className="Contact">
-            <div className="ContactHeadline">Contact Us Now!</div>
+          <div className="ContactHeadline">Contact Us Now!</div>
+          <div className="ContactContent">Feel free to contact us through email or message us on Facebook for booking or any questions.<br/><br/>
+          theboardwalksound@gmail.com
+          <div className="FbHome">
+              <a
+                href="https://www.facebook.com/boardwalksound/"
+                target="_blank"
+              > 
+                  <br/>
+                  https://www.facebook.com/boardwalksound/
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
